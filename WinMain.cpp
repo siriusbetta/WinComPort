1df08f2e3304619547110d845db3d851052fb89b
siriusbetta <aleksey.konyshev@gmail.com>
2015-06-09 16:34:36 +0300

    added some buttons

--- WinMain.cpp
+++ WinMain.cpp
@@ -10,6 +10,16 @@
 #define ID_LIST_PORT			1005
 #define ID_PORT_OPEN_BUTTON		1006
 
+#define ID_LED1_CHECKBOX		1007
+#define ID_LED2_CHECKBOX		1008
+#define ID_LED3_CHECKBOX		1009
+#define ID_ADC_ON_OFF			1010
+
+TCHAR ADC_MessageOff[] = _T("АЦП ВЫКЛ");
+TCHAR ADC_MessageOn[]  = _T("АЦП ВКЛ");
+TCHAR szMessage[] = _T("");
+bool ADC_WorkFlag = false;
+
 HINSTANCE hThisInstance;
 TCHAR WinName[] = _T("WinPort");
 TCHAR WinHead[] = _T("WinComPort terminal");
@@ -21,6 +31,14 @@ HANDLE hSerial; //Serial port
 DCB dcb;//Serial port preferences
 int SizeBuffer = 1200;
 COMMTIMEOUTS CommTimeOuts;
+
+VOID Error(CONST HANDLE hStdOut, CONST LPCSTR szMessage) {
+  DWORD dwTemp;
+  TCHAR szError[256];
+  MessageBox(NULL, szMessage, "Error", MB_OK);
+  ExitProcess(0);
+}
+
 
 int APIENTRY _tWinMain(HINSTANCE hThisInstance,
 					   HINSTANCE hPrevInstance,
@@ -47,10 +65,7 @@ int APIENTRY _tWinMain(HINSTANCE hThisInstance,
 	hWnd = CreateWindow(WinName,
 						WinHead,
 						WS_OVERLAPPEDWINDOW,
-						100,
-						50,
-						500,
-						350,
+						100, 50, 600, 500,
 						HWND_DESKTOP,
 						NULL,
 						hThisInstance,
@@ -70,7 +85,9 @@ int APIENTRY _tWinMain(HINSTANCE hThisInstance,
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	static HWND hSendEdit, hSendButton, hGetEdit, hGetButton, hLabelSend, hLabelGet, hPortList;
+	static HWND hADC_Button, hLED1_Button, hLED2_Button, hLED3_Button;
 	static HWND hPortOpenButton;
+
 	TCHAR hBuff[80];
 	TCHAR str[] = _T("");
 	int cnt = 0;
@@ -79,16 +96,81 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	{
 	case WM_CREATE:
 		{
-		CreateWindow("static", "Введите строку и нажмите Send", WS_CHILD|WS_VISIBLE|SS_CENTER,
+			CreateWindow("static", "АЦП (В)", WS_CHILD|WS_VISIBLE|SS_CENTER,
 						50,30,300,20,
 						hWnd,
 						(HMENU) 0,
 						hThisInstance,
 						NULL);
 
-		hSendEdit = CreateWindow("edit", NULL,
+			hADC_Button = CreateWindow("button", ADC_MessageOn,
+						WS_CHILD|WS_VISIBLE|BS_PUSHBUTTON,
+						267,60,80,20,
+						hWnd,
+						(HMENU) ID_ADC_ON_OFF,
+						hThisInstance,
+						NULL
+						);
+			
+			CreateWindow("static", "LED 1", WS_CHILD|WS_VISIBLE|SS_LEFT,
+						50,90,300,20,
+						hWnd,
+						(HMENU) 0,
+						hThisInstance,
+						NULL);
+
+			hLED1_Button = CreateWindow("button", "Зажги меня",
+						WS_CHILD|WS_VISIBLE|BS_AUTOCHECKBOX,
+						120,90,110,20,
+						hWnd,
+						(HMENU) ID_LED1_CHECKBOX,
+						hThisInstance,
+						NULL
+						);
+
+			CreateWindow("static", "LED 2", WS_CHILD|WS_VISIBLE|SS_LEFT,
+						50,120,300,20,
+						hWnd,
+						(HMENU) 0,
+						hThisInstance,
+						NULL);
+
+			hLED2_Button = CreateWindow("button", "Зажги меня",
+						WS_CHILD|WS_VISIBLE|BS_AUTOCHECKBOX,
+						120,120,110,20,
+						hWnd,
+						(HMENU) ID_LED2_CHECKBOX,
+						hThisInstance,
+						NULL
+						);
+
+			CreateWindow("static", "LED 3", WS_CHILD|WS_VISIBLE|SS_LEFT,
+						50,150,40,20,
+						hWnd,
+						(HMENU) 0,
+						hThisInstance,
+						NULL);
+
+			hLED3_Button = CreateWindow("button", "Зажги меня",
+						WS_CHILD|WS_VISIBLE|BS_AUTOCHECKBOX,
+						120,150,110,20,
+						hWnd,
+						(HMENU) ID_LED3_CHECKBOX,
+						hThisInstance,
+						NULL
+						);
+
+			CreateWindow("static", "Введите строку и нажмите Send", WS_CHILD|WS_VISIBLE|SS_CENTER,
+						50,240,300,20,
+						hWnd,
+						(HMENU) 0,
+						hThisInstance,
+						NULL);
+
+
+		hSendEdit = CreateWindow("checkbox", NULL,
 						WS_CHILD|WS_VISIBLE|WS_BORDER|ES_LEFT,
-						50, 60, 230, 20,
+						50,170,300,20,
 						hWnd,
 						(HMENU) ID_SENT_EDIT,
 						hThisInstance,
@@ -96,7 +178,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
 		hSendButton = CreateWindow("button", "SEND",
 						WS_CHILD|WS_VISIBLE|BS_PUSHBUTTON,
-						300, 60, 50, 20,
+						300, 280, 50, 20,
 						hWnd,
 						(HMENU) ID_SENT_BUTTON,
 						hThisInstance,
@@ -104,7 +186,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 						);
 
 		CreateWindow("static", "Нажмите Get", WS_CHILD|WS_VISIBLE|SS_CENTER,
-						50,120,300,20,
+						50,320,300,20,
 						hWnd,
 						(HMENU) 0,
 						hThisInstance,
@@ -112,7 +194,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
 		hGetEdit = CreateWindow("edit", NULL,
 						WS_CHILD|WS_VISIBLE|WS_BORDER|ES_LEFT,
-						50, 150, 230, 20,
+						50, 360, 230, 20,
 						hWnd,
 						(HMENU) ID_GET_EDIT,
 						hThisInstance,
@@ -120,7 +202,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
 		hGetButton = CreateWindow("button", "GET",
 						WS_CHILD|WS_VISIBLE|BS_PUSHBUTTON,
-						300, 150, 50, 20,
+						300, 360, 50, 20,
 						hWnd,
 						(HMENU) ID_GET_BUTTON,
 						hThisInstance,
@@ -129,7 +211,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
 		hPortList = CreateWindow("listbox", NULL, 
 						WS_CHILD|WS_VISIBLE|LBS_STANDARD|LBS_WANTKEYBOARDINPUT,
-						50, 200, 50, 20,
+						50, 400, 80, 50,
 						hWnd, (HMENU) ID_LIST_PORT,
 						hThisInstance,
 						NULL);
@@ -137,15 +219,40 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		hPortOpenButton = CreateWindow("button",
 						_T("Connect"),
 						WS_CHILD|WS_VISIBLE|BS_PUSHBUTTON,
-						120, 200, 70, 20,
+						150, 400, 70, 20,
 						hWnd,
 						(HMENU) ID_PORT_OPEN_BUTTON,
 						hThisInstance,
 						NULL
 						);
-		return 0;
-		 }
 
+		DWORD cbNeeded = 0,cReturned = 0;
+		//TCHAR str[] = _T("");
+		std::string strName;
+		EnumPorts(0,1,0,0,&cbNeeded,&cReturned); // Detection of the necessary memory amount
+		if (cbNeeded)
+		{
+			unsigned char* info = new unsigned char[cbNeeded];
+
+			if(EnumPorts(0, 1, info, cbNeeded, &cbNeeded, &cReturned )) // Reading the information about ports
+			{
+				PORT_INFO_1* pi = (PORT_INFO_1*)info; // filling the structure
+				
+				for(int i = 0; i < cReturned; i++)
+				{
+					//MessageBox(NULL, _T("hh"), _T("sdf"), MB_OK);
+					strName = pi->pName;
+					if(strName.find("COM") != -1)
+						SendMessage(hPortList, LB_ADDSTRING, 0, (LPARAM)strName.c_str());
+					pi++;
+				}
+				
+			}
+		
+		
+		return 0;
+		}
+		}
 	case WM_COMMAND:
 		{
 			switch(LOWORD(wParam))
@@ -168,6 +275,30 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 						cnt++;
 						nBuffer.clear();
 					}while(true);
+					return 0;
+				}
+			case ID_ADC_ON_OFF:
+				{
+					if(!ADC_WorkFlag)
+					{
+						SetWindowText((HWND) lParam, ADC_MessageOff);
+						ADC_WorkFlag = true;
+						return 0;
+					}
+
+					if(ADC_WorkFlag)
+					{
+						SetWindowText((HWND) lParam, ADC_MessageOn);
+						ADC_WorkFlag = false;
+						//MessageBox(NULL,"Из АЦП","Error",MB_OK);
+						return 0;
+					}
+
+					if(ADC_WorkFlag)
+					{
+
+					}
+					
 					return 0;
 				}
 			case ID_GET_BUTTON:
@@ -206,29 +337,11 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
 					if(hSerial==INVALID_HANDLE_VALUE)
 					{
-						MessageBox(NULL,"Не возможно открыть последовательный порт","Error",MB_OK);
+						wsprintf(szMessage, TEXT("Не возможно открыть последовательный порт"));
+						//MessageBox(NULL,"Не возможно открыть последовательный порт","Error",MB_OK);
 						ExitProcess(1);
 					}
-					/*
-					DWORD iSize;
-					char sReceivedChar[1];
-					BOOL flag = true;
-					std::string test = "";
-
 					
-					{
-						ReadFile(hSerial, sReceivedChar,sizeof(sReceivedChar), &iSize, NULL);//sizeof(sReceivedChar)/8
-							if(iSize == 0) flag = false;
-							test = test + sReceivedChar[0];
-							//cnt++;
-							//abs += sReceivedChar;
-							//MessageBox(NULL,sReceivedChar,"Succesfull",MB_OK);
-					}while(sReceivedChar[0] != 0);
-					MessageBox(NULL,test.c_str(),"Succesfull",MB_OK);
-					//MessageBox(NULL,str,"Succesfull",MB_OK);
-					// получаем 1 байт
-            
-					*/
 					return 0;
 				}
 
